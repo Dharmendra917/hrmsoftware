dce6e0b5b4c4dcdea0715b2ab2c61d756a28f9c6
Dharmedra <dp903604@gmail.com>
2024-03-05 16:20:30 +0530

    change http

--- utils/SendToken.js
+++ utils/SendToken.js
@@ -2,9 +2,11 @@ exports.SendToken = (employee, statuscode, res) => {
   const token = employee.getjwttoken();
 
   const option = {
-    exipres: new Date(Date.now() + process.env.COOKIE_EXIPRES + 24 * 60 * 1000),
+    exipres: new Date(
+      Date.now() + process.env.COOKIE_EXIPRES * 24 * 60 * 60 * 1000
+    ),
     httpOnly: true,
-    // secure: true,
+    secure: true,
   };
   res
     .status(statuscode)
